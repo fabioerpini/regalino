dc4a7c02e4ddab6cf3291976bad72fc72acbc1df
Fabio Erpini <erpini.1936274@studenti.uniroma1.it>
2025-11-09 11:10:27 +0100

    Rimuovi stili non utilizzati e semplifica la struttura dei componenti nel CSS e nel TSX

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import './App.css'
 function App() {
 
   const [active, setActive] = useState<boolean | null>(null);
+  const [selectedDestinazione, setSelectedDestinazione] = useState<number | null>(null);
+
+  function handleDestinazioneClick(id: number) {
+    setSelectedDestinazione(id);
+  }
 
   function startExperience() {
     setActive(true);
@@ -26,22 +31,17 @@ function App() {
 
   ]
     
-  
-
-  
-
-
-
-
-
   return (
     <>
       {active === null && ( // prima schermata
         <>
           <h1>Complimenti Dottoressa!</h1>
-          <div className="card">
-            <button onClick={() => startExperience()}
-            >Scopri ora il tuo regalino!</button>
+          <div className="card-container">
+
+            <div className="card-button">
+              <button onClick={() => startExperience()}
+              >Scopri ora il tuo regalino!</button>
+            </div>
           </div>
           
           <p className="read-the-docs">
@@ -49,26 +49,29 @@ function App() {
           </p>
         </>
       )}
-      {active && (
+      {active && selectedDestinazione === null && (
         <>
           <h1>Scegli la prossima meta</h1>
-          <div className="card">
+          <div className="card-container">
             {destinazioni.map((destinazione) => (
-              
-              <button id={destinazione.id.toString()} className="card-button">
-                {destinazione.titolo}
-              </button>
+              <div className="card" key={destinazione.id}>
+                <button key={destinazione.id} onClick={() => handleDestinazioneClick(destinazione.id)}>
+                  {destinazione.titolo}
+                </button>
+              </div>
             ))}
           </div>
         </>
       )}
-
-
-
-
-
+      {active && selectedDestinazione !== null && (
+        <>
+          <h1>Hai scelto {destinazioni.find(d => d.id === selectedDestinazione)?.titolo}!</h1>
+          <button onClick={() => {  setSelectedDestinazione(null); }}>
+            Torna indietro
+          </button>
+        </>
+      )}
     </>
-    
   )
 }
 
