be01232843a345f40568cc8bef1eb2b3f68fb884
Fabio Erpini <erpini.1936274@studenti.uniroma1.it>
2025-12-11 10:49:56 +0100

    fix aspect ratio camera input

--- src/App.tsx
+++ src/App.tsx
@@ -123,6 +123,30 @@ async function generaPDF( imgPDF: string, cameraInput?: string) {
 
   doc.addImage(imgPDF, 'JPEG', 0, 0, 210, 297);
   if (cameraInput) {
+    // crop image to square
+    cameraInput = await new Promise<string>((resolve) => {
+      const img = new Image();
+      img.src = cameraInput!;
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        const size = Math.min(img.width, img.height);
+        canvas.width = size;
+        canvas.height = size;
+        const ctx = canvas.getContext('2d')!;
+        ctx.drawImage(
+          img,
+          (img.width - size) / 2,
+          (img.height - size) / 2,
+          size,
+          size,
+          0,
+          0,
+          size,
+          size
+        );
+        resolve(canvas.toDataURL('image/jpeg'));
+      };
+    });
     doc.addImage(cameraInput, 'JPEG', 68, 131, 75, 70);
 
   }
