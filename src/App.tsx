02e840d5d6b69d8882f095dc61935f0f60bf8dd9
Fabio Erpini <erpini.1936274@studenti.uniroma1.it>
2025-12-11 10:43:08 +0100

    rotate 90 degrees

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import { base } from 'framer-motion/client';
 
 function App() {
   const [active, setActive] = useState<boolean | null>(null);
@@ -123,7 +124,7 @@ async function generaPDF( imgPDF: string, cameraInput?: string) {
 
   doc.addImage(imgPDF, 'JPEG', 0, 0, 210, 297);
   if (cameraInput) {
-    doc.addImage(cameraInput, 'JPEG', 68, 131, 75, 70);
+    doc.addImage(cameraInput, 'JPEG', 68, 131, 75, 70, undefined, undefined, 90);
 
   }
   
